b40c8fb7cac1c2ee4c9923b15ad9c537b8352a0e
maxirmx <m.samsonov@computer.org>
2023-11-10 14:00:05 +0300

    Created the project and the repo

---
+++ lib/tebako/benchmarking.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "benchmarking/version"
+
+module Tebako
+  module Benchmarking
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/tebako/benchmarking/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Tebako
+  module Benchmarking
+    VERSION = "0.1.0"
+  end
+end
